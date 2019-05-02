aacab2a57ac9e63c39b497ca83716d1c466fdf9e
nikhileuppal <31994837+nikhileuppal@users.noreply.github.com>
2019-05-02 14:16:25 -0400

    Update and rename EditCell.java to People.java

---
+++ People.java
@@ -0,0 +1,100 @@
+package tableviewfxmlexample;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class Person {
+
+    private SimpleStringProperty id;
+    private SimpleStringProperty name;
+    private SimpleStringProperty age;
+    private final SimpleStringProperty email;
+
+    private SimpleStringProperty primary;
+
+    private SimpleStringProperty secondry;
+
+
+    Person(String id, String name, String age, String email, String primary, String secondry) {
+
+        this.id = new SimpleStringProperty(id);
+        this.name = new SimpleStringProperty(name);
+        this.age = new SimpleStringProperty(age);
+        this.email = new SimpleStringProperty(email);
+
+        this.primary = new SimpleStringProperty(primary);
+
+        this.secondry = new SimpleStringProperty(secondry);
+
+    }
+
+
+    public String getSecondry() {
+        return secondry.get();
+    }
+
+
+    public void setSecondry(String secondry) {
+        this.secondry.set(secondry);
+    }
+
+
+    public String getPrimary() {
+        return primary.get();
+    }
+
+
+    public void setPrimary(String primary) {
+        this.primary.set(primary);
+    }
+
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id.get();
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id.set(id);
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name.get();
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    /**
+     * @return the age
+     */
+    public String getAge() {
+        return age.get();
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(String age) {
+        this.age.set(age);
+    }
+
+    public String getEmail() {
+        return email.get();
+    }
+
+    public void setEmail(String email) {
+        this.email.set(email);
+    }
+}
